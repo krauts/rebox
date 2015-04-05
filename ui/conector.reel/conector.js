aaf100f8ded14a9d4453d8172051bccf3cfd80d5
krauts <noreply>
2015-04-05 20:40:12 +0000

    Add component conector

---
+++ ui/conector.reel/conector.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/conector.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Conector
+ * @extends Component
+ */
+exports.Conector = Component.specialize(/** @lends Conector# */ {
+    constructor: {
+        value: function Conector() {
+            this.super();
+        }
+    }
+});
